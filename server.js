dbeeda054d83c723909322cb4cb6c57ada8ffcf0
Rokon <rokon@itebd.com>
2020-08-25 21:43:41 +0600

    dfaffs

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Nodejs webapp deployed by Kubernetes. Md. Rokon. Dhaka International University.\n');
+  res.send('Nodejs webapp deployed by Kubernetes.\n');
 });
 
 app.listen(PORT, HOST);
